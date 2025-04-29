235a3e2c4e43e41b490a73defc02f8a06feb1fd7
kosa\kosa1 <subin4420@gmail.com>
2025-04-29 16:22:17 +0900

    Revert "init: 세미프로젝트까지의 Spring Boot"
    This reverts commit eb7e8d2d481666770c92e767d10757e1c393937a.

--- demo/demo/src/main/java/com/miniflex/demo/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.miniflex.demo.repository;
-
-import com.miniflex.demo.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
